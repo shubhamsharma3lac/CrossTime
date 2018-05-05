e5ca793e28d76e3d2f74d733be3c0e641e99d70f
shubhamsharma3lac <38815887+shubhamsharma3lac@users.noreply.github.com>
2018-05-05 16:37:22 +0530

    1

---
+++ script/main.js
@@ -0,0 +1,19 @@
+function init(){
+    var button = document.querySelector(".nav-bar-menu-button");
+    button.addEventListener("click", showSlidingPanel, false);
+}
+
+function showSlidingPanel()
+{
+    var  slideBar = document.querySelector(".slide-nav-bar")
+    slideBar = slideBar == null ? document.querySelector(".slide-nav-bar-active"): slideBar;
+    var className = slideBar.className;
+    if(className == "slide-nav-bar")
+    {
+        slideBar.className = "slide-nav-bar-active";
+    }
+    else
+    {
+        slideBar.className = "slide-nav-bar";
+    }
+}
